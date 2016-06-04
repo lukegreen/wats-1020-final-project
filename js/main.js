87f1c39d83e6b89c07ce5772831d11772b3df64b
elukuent <elukuent@gmail.com>
2016-06-04 18:30:46 -0400

    fixed bug

--- js/main.js
+++ js/main.js
@@ -44,49 +44,5 @@ $(document).ready(function(){
     offset: '10%'
 });
   
-  //Architect details event listener
-  
-  $('.view-details').on('click', function(event){
-    var targetElement = event.target;
-    var container = targetElement.parentElement.parentElement;
-    $(container).find('.details').each(function (index, el){
-      if($(el).is(':visible')) {
-        $(el).fadeOut();
-        targetElement.innerHTML = "View Details";
-      } else {
-        $(el).fadeIn();
-        targetElement.innerHTML = "Hide Details";
-      }
-    });
-  });
-  
-
-  //Featurette image scroll-triggered fadeIn
-  //utilizing Waypoints plug-in: http://imakewebthings.com/waypoints/
-  
-  var waypoints = $('#waypoint-one').waypoint({
-  handler: function() {
-    console.log("success!");
-    $('#featurette-img-one').fadeIn(3000);
-  },
-    offset: '25%'
-});
-  
-   var waypoints2 = $('#waypoint-two').waypoint({
-  handler: function() {
-    console.log("success!");
-    $('#featurette-img-two').fadeIn(3000);
-  },
-    offset: '25%'
-});
-  
-   var waypoints3 = $('#waypoint-three').waypoint({
-  handler: function() {
-    console.log("success!");
-    $('#featurette-img-three').fadeIn(3000);
-  },
-    offset: '10%'
-});
-  
   
 });
